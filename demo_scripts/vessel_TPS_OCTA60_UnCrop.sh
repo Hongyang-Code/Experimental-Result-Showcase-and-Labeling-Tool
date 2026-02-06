f95ce3a65a5c8b53ba9cd1edca4bd9eec9f825c2
Cangshanqingshi <347604242@qq.com>
2026-02-07 01:48:42 +0800

    chore: stop tracking environment-specific scripts and results

--- demo_scripts/vessel_TPS_OCTA60_UnCrop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 用法: ./scripts/start.sh [i/v/t] [数据路径] [标签路径]
-# 示例: ./scripts/start.sh i ./exp_result ./label
-data_type="i"
-data_dir="/data2/lhy/project/GeoFormer-main/Visualization/ResultVis/result_tps_vis_UnCrop"
-label_dir="/data2/lhy/project/GeoFormer-main/Visualization/ResultVis/result_tps_vis_UnCrop/label"
-port="10009"
-
-python app.py $data_type $data_dir $label_dir $port
